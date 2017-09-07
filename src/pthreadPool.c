55a3bef2a4a308fac594a309a801fab5fb00890e
BambooAce <1774390393@qq.com>
2017-09-07 18:02:50 +0800

    add the module of thread pool

--- src/pthreadPool.c
+++ src/pthreadPool.c
@@ -1,1 +1,33 @@
-#include "pthread.h"
+#include "task.h"
+#include <unistd.h>
+#define MAX 300
+pthread_t Threads[MAX];
+
+static void * run(TASKLIST *tl)
+{
+    if(tl)
+    {
+        TASKPACKAGE tkg;
+        while(1){
+            if(getATask(tl, &tkg))
+            {
+                tkg.et(tkg.arg);
+            }else{
+                usleep(800);
+            }
+        }
+    }
+}
+
+int initThreadPool()
+{
+    int i;
+    for(i = 0; i < MAX; ++i)
+    {
+        Threads[i] = 0;
+    }
+    for(;;)
+    {
+        pthread_create(&Threads[i], NULL, run, NULL);
+    }
+}
