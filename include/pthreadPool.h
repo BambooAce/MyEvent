fc4f52900974f4f42c14e3d7d5a6ab514560e685
BambooAce <1774390393@qq.com>
2017-09-07 18:01:38 +0800

    add task.h header file

--- include/pthreadPool.h
+++ include/pthreadPool.h
@@ -1,21 +1,3 @@
+#include <pthread.h>
 #include "task.h"
-
-
-InitPool();
-
-run()
-{
-    while(1){
-        if(getTaskNum(tl))
-        {
-            getATask(tl);
-        }else{
-            sleep(4);
-        }
-    }
-}
-
-
-
-putATask(TASKLIST *tl, ExecuteTask runtask, void *arg, unsigned int argsize, int priority);
-
+int initThreadPool();
