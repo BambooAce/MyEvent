58384a480a36cc367d4cb05c61330055be86440a
BambooAce <1774390393@qq.com>
2017-09-08 18:10:31 +0800

    update

--- include/task.h
+++ include/task.h
@@ -1,11 +1,14 @@
+#ifndef TASK_H
+#define TASK_H
 #include <pthread.h>
 
-typedef void (*ExecuteTask)(void **arg);
+typedef void (*ExecuteTask)(void *arg);
 
 typedef struct taskpackage{
     void * arg;
+    unsigned int argsize;
     ExecuteTask et;
-    int status; //0 in list no call, 1 get from list, 2 running
+    int status; //0 in list no call, 1 get from list and running
 }TASKPACKAGE;
 
 typedef struct TASKNODE{
@@ -17,7 +20,7 @@ typedef struct TASKNODE{
 typedef struct {
     TaskNode * header;
     unsigned int num;
-    pthread_mutex_t * mutex;
+    pthread_mutex_t  mutex;
 }TASKLIST;
 
 int initTaskList(TASKLIST *tl);
@@ -25,4 +28,6 @@ int getATask(TASKLIST *tl, TASKPACKAGE *tpkg);
 int putATask(TASKLIST *tl, ExecuteTask runtask, void *arg, unsigned int argsize);
 void deleteTask(TASKLIST *tl, ExecuteTask _tg);
 void clearTaskList(TASKLIST *tl);
-int getTaskNum(TASKLIST * tl);
+void getRunningTaskNum(TASKLIST * tl, int *tasknum);
+
+#endif // TASK_H
