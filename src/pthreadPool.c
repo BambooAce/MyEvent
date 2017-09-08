42196d09f60cd5ee381a3afb839b54051b9df7ea
BambooAce <1774390393@qq.com>
2017-09-08 18:09:20 +0800

    add thread

--- src/pthreadPool.c
+++ src/pthreadPool.c
@@ -2,9 +2,18 @@
 #include <unistd.h>
 #define MAX 300
 pthread_t Threads[MAX];
-
-static void * run(TASKLIST *tl)
+pthread_attr_t attr;
+/**
+ * @brief run
+ * @param arg
+ * @return
+ * thread callback function, every thread get a task from task list
+ * if get task then run it, or sleep
+ * If you want get return value, you can convert argument into arg.
+ */
+static void * run(void *arg)
 {
+    TASKLIST *tl = (TASKLIST *)arg;
     if(tl)
     {
         TASKPACKAGE tkg;
@@ -13,21 +22,55 @@ static void * run(TASKLIST *tl)
             {
                 tkg.et(tkg.arg);
             }else{
-                usleep(800);
+                usleep(50000);
             }
         }
     }
 }
 
-int initThreadPool()
+/**
+ * @brief initThreadPool
+ * @param threadnum
+ * @param tl
+ * @param detach
+ * @return
+ * Initial thread pool, you can choose detach or not.
+ */
+int initThreadPool(int threadnum, TASKLIST * tl, int detach)
 {
     int i;
     for(i = 0; i < MAX; ++i)
     {
         Threads[i] = 0;
     }
-    for(;;)
+    if(detach)
     {
-        pthread_create(&Threads[i], NULL, run, NULL);
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+    }
+    for(i = 0;i < threadnum; ++i)
+    {
+        if( -1 == pthread_create(&Threads[i], (detach ? &attr: NULL), run, (void *)tl))
+        {
+            break;
+        }
+    }
+    return 1;
+}
+
+/**
+ * @brief joinAllThreads
+ * @return
+ * Wait all threads exit and collect status
+ */
+int joinAllThreads()
+{
+    int i;
+    for(i = 0; i < MAX; ++i)
+    {
+        if(Threads[i] > 0)
+        {
+            pthread_join(Threads[i], NULL);
+        }
     }
 }
